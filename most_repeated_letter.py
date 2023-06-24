a429eb23bda36d7fa8480ba32cace0f12b37005f
ValeriiHolubiev <118043064+ValeriiHolubiev@users.noreply.github.com>
2023-06-24 17:03:06 +0200

    Update most_repeated_letter.py

--- most_repeated_letter.py
+++ most_repeated_letter.py
@@ -1,10 +1,14 @@
+#    This program finds the most repeated letter in the string.
+
 text = "This is a common interview question"
 
-dict = {letter:0 for letter in text if letter != ' '}
+dict = {letter:0 for letter in text if letter != ' '}    # Here I'm using comprehension to create dictionary, where letters are keys, and their quantities are values
 
-for l in text:
+for l in text:    # Here I'm going through the string using a loop and count how many and what letters the string has
     if l != ' ':
         dict[l] += 1
 
-print(f"Most repeated character in the text is '{sorted(dict.items(), key = lambda x:x[1], reverse = True)[0][0]}', "
-      f"it repeats {sorted(dict.items(), key = lambda x:x[1], reverse = True)[0][1]} times")
+print(f"Most repeated character in the text is '{sorted(dict.items(), key = lambda x:x[1], reverse = True)[0][0]}', "    # Here I'm retrieving keys and values of my dictionary with itmes() method
+      f"it repeats {sorted(dict.items(), key = lambda x:x[1], reverse = True)[0][1]} times")                             # then I'm using the lambda function that returns quantities of letters
+                                                                                                                         # then I'm sorting my dictionary by letters quantities with reverse flag 
+                                                                                                                         # and getting the first item of my sorted dictionary
